6f60b074258943baebb2363b2e925285758f3084
NsavelevJs <nsavelevjs@gmail.com>
2020-12-22 13:38:42 -0500

    Fixed tailwindcss by adding an output file and added it to App.js and an import

--- hungerswipe/src/App.js
+++ hungerswipe/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-
+import "./tailwind.output.css";
 function App() {
   return (
     <div className="App">
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="bg-black"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
