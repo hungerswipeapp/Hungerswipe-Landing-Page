01986810fc4950680aa7b819af3a16f9e8089228
NsavelevJs <nsavelevjs@gmail.com>
2021-01-05 12:03:05 -0500

    Added a Header, Footer and main content page. Working out functionality on the nav currently.

--- hungerswipe/src/App.js
+++ hungerswipe/src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 import "./tailwind.output.css";
 import Header from "./Shared/Header";
+import Main from "./Main";
+import Footer from "./Shared/Footer"
 
 function App() {
   return (
     <div>
       <Header/>
+      <Main/>
+      <Footer/>
     </div>
   );
 }
--- hungerswipe/src/Shared/Header.jsx
+++ hungerswipe/src/Shared/Header.jsx
@@ -1,61 +1,118 @@
-import React, { Component } from "react";
-import $ from "jquery";
-class Header extends Component {
-  componentDidMount() {
-    var click = false;
-    $(document).on("click", ".hamburger--3dx", function () {
-      click = !click;
-      if (click) {
-        $(this).addClass("is-active");
-        $("#dropDown").hide().animate({
-          height: "toggle",
-        });
-      } else {
-        $(this).removeClass("is-active");
-        $("#dropDown").show().animate({
-          height: "toggle",
-        });
-      }
-    });
-  }
-  render() {
-    return (
-      <div class="container">
-        <div class="row">
-          <div id="header" class="col-lg-12">
-            <div class="pull-left">
-              <h1>
-                HungerSwipe logo here
-              </h1>
-            </div>
-            <div class="pull-right">
-              <div class="hamburger hamburger--3dx">
-                <div class="hamburger-box">
-                  <div class="hamburger-inner"></div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+import React from "react";
 
-        <div id="dropDown" class="row">
-          <div class="col-lg-12">
-            <ul class="list list-unstyled text-center">
-              <li>
-                <a href="#">Portfolio</a>
+
+function Header () {
+
+  let scrollpos = window.scrollY;
+  const header = document.getElementById("header");
+  const navcontent = document.getElementById("nav-content");
+  const navaction = document.getElementById("navAction");
+  const brandname = document.getElementById("brandname");
+  const toToggle = document.querySelectorAll(".toggleColour");
+
+  document.addEventListener("scroll", function () {
+    /*Apply classes for slide in bar*/
+    scrollpos = window.scrollY;
+
+    if (scrollpos > 10) {
+      header.classList.add("bg-white");
+      navaction.classList.remove("bg-white");
+      navaction.classList.add("gradient");
+      navaction.classList.remove("text-gray-800");
+      navaction.classList.add("text-white");
+      //Use to switch toggleColour colours
+      for (let i = 0; i < toToggle.length; i++) {
+        toToggle[i].classList.add("text-gray-800");
+        toToggle[i].classList.remove("text-white");
+      }
+      header.classList.add("shadow");
+      navcontent.classList.remove("bg-gray-100");
+      navcontent.classList.add("bg-white");
+    } else {
+      header.classList.remove("bg-white");
+      navaction.classList.remove("gradient");
+      navaction.classList.add("bg-white");
+      navaction.classList.remove("text-white");
+      navaction.classList.add("text-gray-800");
+      //Use to switch toggleColour colours
+      for (let i = 0; i < toToggle.length; i++) {
+        toToggle[i].classList.add("text-white");
+        toToggle[i].classList.remove("text-gray-800");
+      }
+
+      header.classList.remove("shadow");
+      navcontent.classList.remove("bg-white");
+      navcontent.classList.add("bg-gray-100");
+    }
+  });
+  
+    return (
+      <nav id="header" class="fixed w-full z-30 top-0 text-white">
+        <div class="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-2">
+          <div class="pl-4 flex items-center">
+            <a
+              class="toggleColour text-white no-underline hover:no-underline font-bold text-2xl lg:text-4xl"
+              href="#"
+            >
+              <img src="./logo-white.png"/>
+            </a>
+          </div>
+          <div class="block lg:hidden pr-4">
+            <button
+              id="nav-toggle"
+              class="flex items-center p-1 text-pink-800 hover:text-gray-900 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out"
+            >
+              <svg
+                class="fill-current h-6 w-6"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <title>Menu</title>
+                <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+              </svg>
+            </button>
+          </div>
+          <div
+            class="w-full flex-grow lg:flex lg:items-center lg:w-auto hidden mt-2 lg:mt-0 bg-white lg:bg-transparent text-black p-4 lg:p-0 z-20"
+            id="nav-content"
+          >
+            <ul class="list-reset lg:flex justify-end flex-1 items-center">
+              <li class="mr-3">
+                <a
+                  class="inline-block py-2 px-4 text-black font-bold no-underline"
+                  href="#"
+                >
+                  Home
+                </a>
               </li>
-              <li>
-                <a href="#">About</a>
+              <li class="mr-3">
+                <a
+                  class="inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4"
+                  href="#"
+                >
+                  About
+                </a>
               </li>
-              <li>
-                <a href="#">Contact</a>
+              <li class="mr-3">
+                <a
+                  class="inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4"
+                  href="#"
+                >
+                  Features
+                </a>
               </li>
             </ul>
+            <button
+              id="navAction"
+              class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full mt-4 lg:mt-0 py-4 px-8 shadow opacity-75 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out"
+            >
+              Early access
+            </button>
           </div>
         </div>
-      </div>
+        <hr class="border-b border-gray-100 opacity-25 my-0 py-0" />
+      </nav>
     );
-  }
 }
 
 export default Header;
