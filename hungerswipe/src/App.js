31edbf22bb99d21ab1803e1cd342e59befa33257
Jalon Dodson <jalon.dodson@yahoo.com>
2021-08-13 16:33:41 -0500

    rewrote website to use nextjs

--- hungerswipe/src/App.js
+++ hungerswipe/src/App.js
@@ -1,15 +1,24 @@
 import "./App.css";
 import "./tailwind.output.css";
 import Main from "./Shared/Main";
-import Footer from "./Shared/Footer"
+import Footer from "./Shared/Footer";
 import Feedback from "./Shared/Feedback";
+import { Helmet } from "react-helmet";
+import appIcon from "./Assets/favicon.ico";
 
 function App() {
   return (
     <>
-      <Main/>
+      <Helmet>
+        <link rel="icon" href={appIcon} />
+        <meta
+          name="description"
+          content="Swipe Right to your next restaurant destination! Match on restaurants with your friends, family or even that one special person in your life."
+        />
+      </Helmet>
+      <Main />
       <Feedback />
-      <Footer/>
+      <Footer />
     </>
   );
 }
